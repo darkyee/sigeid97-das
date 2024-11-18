d953a2aafe56ca59ee892b5ba8b5f4a426f97670
Darkyee <52757313+darkyee@users.noreply.github.com>
2024-11-18 10:47:55 -0600

    Update src/app/components/AppFooter.js [BoltSync]

---
+++ src/app/components/AppFooter.js
@@ -0,0 +1,13 @@
+export default function AppFooter() {
+    return (
+        <footer className="">
+            <div className="container py-3">
+                <div className="row">
+                    <div className="col">
+                        <p className="text-center fw-bold m-0">© 2024 SIGEID</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
